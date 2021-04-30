4e0d4cabd12866000aa32b8c0edae8f884174031
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-05-01 02:59:43 +0530

    committing files

---
+++ rust_scripts/uint.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Unsigned integer is nomrally used when 
+    println!(" THE UNSIGNED INTEGER Is used if I am sure that no negative numbers will be used...");
+    let x : u32 = 4999;
+    println!(" The Value of x = {}", x);
+
+    let f: f32 = 4.5; // float 32 bit;
+    let b: bool = false; // boolean
+    println!("First is a float and the next is a boolian ---- {}   {}", f, b);
+}
