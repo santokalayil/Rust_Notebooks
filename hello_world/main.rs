9afac1a6dab9b4fb6250b98698b766595fc4e7bd
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-05-01 02:57:20 +0530

    committing files

---
+++ hello_world/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // The macro prints the string written here
+    /* 
+    this is
+    a multiline comment
+    */
+    println!("========================================================================================");
+    println!("Used Command - 'rustc main.rs' after creating a directory and just ran .. ");
+    println!("========================================================================================");
+}
