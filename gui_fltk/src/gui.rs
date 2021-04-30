f7ceffd1dfd8e4c672cad6fc8c0b8ba983d70e93
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-05-01 02:51:18 +0530

    committing files

---
+++ gui_fltk/src/gui.rs
@@ -0,0 +1,24 @@
+use fltk::{app::*, button::*, frame::*, window::*, image::*};  //
+mod gui_frame;
+
+#[derive(RustEmbed)]
+#[folder = "assets/"]
+struct Assets;
+
+pub fn start_app() {
+    let app = App::default();
+    let mut win = Window::new(100, 100, 400, 300, "Santo's App",);
+
+    let img = Assets::get("app_icon.png").unwrap(); // these 2 lines to embed if not use line after next line only
+    let image = PngImage::from_data(&img).unwrap();
+
+    // let image = PngImage::load(&std::path::Path::new("assets/app_icon.png")).unwrap();
+
+    win.set_icon(Some(image));
+    let mut frame = Frame::new(0, 0, 400, 200, "");
+    let mut but = Button::new(160, 210, 80, 40, "Click me!");
+    win.end();
+    win.show();
+    but.set_callback(move || frame.set_label("You clicked the Button!"));
+    app.run().unwrap();
+}
